d0e7b953ed1aa4a42e137e9961f31e9f41038de8
Yair292 <dluquep@unjbg.edu.pe>
2024-10-19 12:25:41 -0500

    Corrigiendo Errores en el proyecto

--- Distancia.cpp
+++ Distancia.cpp
@@ -5,9 +5,9 @@ using namespace std;
 void Distancia() {
 	system("cls");
 	double velocidad,tiempo;
-    cout<<"Ingrese la velocidad (km/h): "; cin>>velocidad;
-    cout<<"Ingrese el tiempo (h): "; cin>>tiempo;
+    cout<<"Ingrese la velocidad (m/s): "; cin>>velocidad;
+    cout<<"Ingrese el tiempo (s): "; cin>>tiempo;
     double distancia=velocidad*tiempo;
-    cout<<"\nLa distancia recorrida es: "<<distancia<<" km\n"<< endl;
+    cout<<"\nLa distancia recorrida es: "<<distancia<<" m/s"<< endl;
     system("pause");
 }
