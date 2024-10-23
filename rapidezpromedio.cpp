1789ef380fac0cda3615762dc4e90250ded6df09
Yair292 <dluquep@unjbg.edu.pe>
2024-10-22 22:32:48 -0500

    Agregar colores

--- rapidezpromedio.cpp
+++ rapidezpromedio.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <windows.h>
 #include "funciones.h"
 
 using namespace std;
@@ -8,9 +9,18 @@ void Rapidezprom() {
 	double d, t1, t2;
 	cout<<"\n\nRapidez promedio=\t\033[4m d \033[0m\n";
 	cout << "         \t\tΔt        \n\n";
-    cout<<"Ingrese la distacia recorrida (m) : "; cin>>d;
-    cout<<"Ingrese el tiempo inicial (s): "; cin>>t1;
-    cout<<"Ingrese el tiempo final (s): "; cin>>t2;
+    cout<<"Ingrese la distacia recorrida (m) : "; 
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+	cin>>d;
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+    cout<<"Ingrese el tiempo inicial (s): "; 
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+	cin>>t1;
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+    cout<<"Ingrese el tiempo final (s): "; 
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+	cin>>t2;
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
     cout<<"Rapidez promedio = ("<<d<<") / "<<"("<<t2<<"-"<<t1<<")\n";
     double rap=(d)/(t2-t1);
     cout<<"La rapidez promedio es : "<<rap<<" m/s\n";
